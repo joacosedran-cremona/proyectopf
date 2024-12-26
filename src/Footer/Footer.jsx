0eca84c1f126b020c4e0e440b8a19978e111c04d
joacosedran <jledesma@creminox.com>
2024-12-26 17:09:04 -0300

    Footer revision

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import creminoxLogo from './creminox-logo.png';
+import cremImg from '../IMG/logos/creminox-logo.png';
+import igImg from '../IMG/logos/instagram.png';
+import linkImg from '../IMG/logos/linkedin.png';
+import locImg from '../IMG/logos/location.png';
+import mailImg from '../IMG/logos/mail.png';
+import phoneImg from '../IMG/logos/telephone.png';
+import fbImg from '../IMG/logos/facebook.png';
+
 import style from './Footer.module.css';
 
 const Footer = () => {
@@ -8,43 +15,47 @@ const Footer = () => {
         <div className={style.contenedor}>
             <ul className={`${style.contenedor} ${style.footerIzq}`}>
                 <li>
-                    <img src="location-icon.png" alt="Ubicación" className={style.icon} />
-                    Beron de Astrada 2745 - Argentina
+                    <img src={locImg} alt="Ubicación" className={style.icon} />
+                    <p>
+                        Beron de Astrada 2745 - Argentina
+                    </p>
                 </li>
                 <li>
-                    <img src="email-icon.png" alt="Correo" className={style.icon} />
+                    <img src={mailImg} alt="Correo" className={style.icon} />
                     <a href="mailto:sistemas@creminox.com">sistemas@creminox.com</a>
                 </li>
                 <li>
-                    <img src="phone-icon.png" alt="Teléfono" className={style.icon} />
-                    4319-3944
+                    <img src={phoneImg} alt="Teléfono" className={style.icon} />
+                    <p>
+                        4319-3944
+                    </p>
                 </li>
             </ul>
 
             <div className={`${style.contenedor} ${style.footerImg}`}>
             <a href="https://creminox.com" target="_blank" rel="noopener noreferrer">
-                <img src={creminoxLogo} alt="Creminox logo" />
+                <img src={cremImg} alt="Creminox logo" />
             </a>
             </div>
 
             <ul className={`${style.contenedor} ${style.footerDer}`}>
                 <li>
-                    <img src="website-icon.png" alt="Sitio web" className={style.icon} />
                     <a href="https://www.creminox.com" target="_blank" rel="noopener noreferrer">
                     www.creminox.com
                     </a>
+                    <img src={linkImg} alt="Linkedin" className={style.icon} />
                 </li>
                 <li>
-                    <img src="facebook-icon.png" alt="Facebook" className={style.icon} />
                     <a href="https://facebook.com/creminox7402" target="_blank" rel="noopener noreferrer">
                     /creminox7402
                     </a>
+                    <img src={fbImg} alt="Facebook" className={style.icon} />
                 </li>
                 <li>
-                    <img src="instagram-icon.png" alt="Instagram" className={style.icon} />
                     <a href="https://instagram.com/Creminox" target="_blank" rel="noopener noreferrer">
                     /Creminox
                     </a>
+                    <img src={igImg} alt="Instagram" className={style.icon} />
                 </li>
             </ul>
         </div>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,21 @@
+import cremImg from '../IMG/logos/creminox.png';
+
 import style from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className={style.header}>
-            <nav className={style.nav}>
+        <header className={style.container}>
+            <nav className={style.navbar}>
             <ul>
-                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#inicio">Home</a></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#inicio">Home</a></li>
                 <li><a href="#sobre">Sobre</a></li>
                 <li><a href="#contacto">Contacto</a></li>
             </ul>
+            <div>
+                <img className={style.img} src={cremImg} alt="Creminox" />
+            </div>
             </nav>
         </header>
     );
