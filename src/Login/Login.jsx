6058db6ee11d413d4c025fdc81671f343f43a649
joacosedran <jledesma@creminox.com>
2024-12-27 12:39:12 -0300

    Login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,21 +1,34 @@
-import cremImg from '../IMG/logos/creminox.png';
+import cremImg from '../IMG/logos/creminox-logo.png';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 import style from './Login.module.css';
 
-const Login = () => {
+function Login() {
     return (
         <>
             <Header />
             <body className={style.contenedor}>
-                <nav className={style.formularioLogin}>
-                    <h5>Login</h5>
-                    <div>
+                    <div className={style.formularioLogin}>
                         <img className={style.img} src={cremImg} alt="Creminox" />
+                        <div className={style.inputUsuario}>
+                            <p className={style.inputsTextos}>Usuario</p>
+                            <input
+                                className={style.inputs} 
+                                type="text"
+                            />
+                        </div>
+                        <div className={style.inputContraseña}>
+                            <p className={style.inputsTextos}>Contraseña</p>
+                            <input 
+                                className={style.inputs}
+                                type="password" 
+                            />
+                        </div>
+                        <button className={style.botonIngresar}>Ingresar</button>
+                        <ul className={style.botonRecuperar}>
+                            <li><a href="#botonRecuperar">Recuperar contraseña</a></li>
+                        </ul>
                     </div>
-                </nav>
             </body>
-            <Footer />
         </>
     );
 };
